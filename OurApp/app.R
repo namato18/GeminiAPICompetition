b2af7fe01cd335c91ef728190d11d5c3e047d7a5
michael j Swallower <nickamato608@gmail.com>
2024-05-30 12:42:47 -0400

    test app

--- OurApp/app.R
+++ OurApp/app.R
@@ -1,51 +1,42 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+Sys.setenv(GEMINI_API_KEY = "xxxxxxxxxxx")
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  mainPanel(
+    fluidRow(
+      fileInput(
+        inputId = "imgFile",
+        label = "Select image to upload",
+      ),
+      textInput(
+        inputId = "prompt", 
+        label = "Prompt", 
+        placeholder = "Enter Your Query"
+      ),
+      actionButton("submit", "Talk to Gemini"),
+      textOutput("response")
+    ),
+    imageOutput(outputId = "myimage")
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  
+  observeEvent(input$imgFile, {
+    path <- input$imgFile$datapath
+    output$myimage <- renderImage({
+      list(
+        src = path
+      )
+    }, deleteFile = FALSE)
+  })
+  
+  observeEvent(input$submit, {
+    output$response <- renderText({
+      gemini_vision(input$prompt, input$imgFile$datapath)
     })
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
