27103ffa2755083208a4933edaf10ad2e0764709
michael j Swallower <nickamato608@gmail.com>
2024-05-30 14:56:35 -0400

    updated funcs file for images

--- GeminiFuncs.R
+++ GeminiFuncs.R
@@ -1,5 +1,6 @@
 library(httr)
 library(jsonlite)
+library(base64enc)
 
 
 api_key <- Sys.setenv(GEMINI_API_KEY = "AIzaSyCFCKa8CM0c_khCk8qBtGZWInPO2pFvKak")
@@ -86,4 +87,57 @@ chat_gemini <- function(prompt,
   return(answer)
   
 }
+
+# Function
+gemini_vision <- function(prompt, 
+                          image,
+                          temperature=0.5,
+                          max_output_tokens=4096,
+                          api_key=Sys.getenv("GEMINI_API_KEY"),
+                          model = "gemini-pro-vision") {
+  
+  if(nchar(api_key)<1) {
+    api_key <- readline("Paste your API key here: ")
+    Sys.setenv(GEMINI_API_KEY = api_key)
+  }
+  
+  model_query <- paste0(model, ":generateContent")
+  
+  response <- POST(
+    url = paste0("https://generativelanguage.googleapis.com/v1beta/models/", model_query),
+    query = list(key = api_key),
+    content_type_json(),
+    encode = "json",
+    body = list(
+      contents = list(
+        parts = list(
+          list(
+            text = prompt
+          ),
+          list(
+            inlineData = list(
+              mimeType = "image/png",
+              data = base64encode(image)
+            )
+          )
+        )
+      ),
+      generationConfig = list(
+        temperature = temperature,
+        maxOutputTokens = max_output_tokens
+      )
+    )
+  )
+  
+  if(response$status_code>200) {
+    stop(paste("Error - ", content(response)$error$message))
+  }
+  
+  candidates <- content(response)$candidates
+  outputs <- unlist(lapply(candidates, function(candidate) candidate$content$parts))
+  
+  return(outputs)
+  
+}
+
 
